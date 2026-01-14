91ffb2b7d5981b6d8f873cf5ff717da826a70d18
akeithuri-dot <akeithuri@gmail.com>
2026-01-14 11:36:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -99,8 +99,6 @@ def users():
     return render_template('users.html', users=users)
 
 # RUN
-
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
